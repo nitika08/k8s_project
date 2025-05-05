3ac1d304ce94ccf3cde42ee223c0999e518f27a9
nitika08 <nitika.singhal8@gmail.com>
2025-05-05 16:41:16 +0530

    Update main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,86 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+# Create Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true
+  tags                = var.tags
+}
+
+# Create AKS cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.aks_dns_prefix
+  kubernetes_version  = var.kubernetes_version
+
+  default_node_pool {
+    name                = "default"
+    node_count          = var.node_count
+    vm_size             = var.node_size
+    os_disk_size_gb     = 30
+    enable_auto_scaling = true
+    min_count           = var.min_node_count
+    max_count           = var.max_node_count
+    vnet_subnet_id      = azurerm_subnet.aks_subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+    service_cidr      = "10.0.0.0/16"
+    dns_service_ip    = "10.0.0.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = var.tags
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.aks_cluster_name}-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+}
+
+# Create subnet
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.0.0/24"]
+}
+
+# Assign AcrPull role to AKS
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
+# Output kubeconfig for easy access
+resource "local_file" "kubeconfig" {
+  content  = azurerm_kubernetes_cluster.aks.kube_config_raw
+  filename = "${path.module}/kubeconfig"
+}
