5e0acc8cd01b16b98174fe65cec24da428968078
nitika08 <nitika.singhal8@gmail.com>
2025-05-05 16:41:41 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,68 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "notes-app-rg"
+}
+
+variable "location" {
+  description = "Azure region to deploy resources"
+  type        = string
+  default     = "eastus"
+}
+
+variable "acr_name" {
+  description = "Name of the Azure Container Registry"
+  type        = string
+  default     = "notesappacr"
+}
+
+variable "aks_cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+  default     = "notes-app-aks"
+}
+
+variable "aks_dns_prefix" {
+  description = "DNS prefix for AKS cluster"
+  type        = string
+  default     = "notesapp"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.27.3"
+}
+
+variable "node_count" {
+  description = "Initial number of nodes in the AKS cluster"
+  type        = number
+  default     = 2
+}
+
+variable "min_node_count" {
+  description = "Minimum number of nodes in the AKS cluster"
+  type        = number
+  default     = 1
+}
+
+variable "max_node_count" {
+  description = "Maximum number of nodes in the AKS cluster"
+  type        = number
+  default     = 5
+}
+
+variable "node_size" {
+  description = "VM size for AKS nodes"
+  type        = string
+  default     = "Standard_D2_v2"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {
+    Environment = "Development"
+    Project     = "NotesApp"
+  }
+}
